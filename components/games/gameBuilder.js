be83271807a92abd8a4f41635520bc4307e4b317
Khmelovskyi, Vladyslav <vladyslav.khmelovskyi@sigma.software>
2023-03-13 01:13:23 +0200

    Add game builder tests with implementation

---
+++ components/games/gameBuilder.js
@@ -0,0 +1,29 @@
+const Game = require("./game");
+
+class GameBuilder {
+  startGame = false;
+  player1 = null;
+  player2 = null;
+
+  withPlayers(player1, player2){
+    this.player1 = player1;
+    this.player2 = player2;
+    return this;
+  }
+
+  withStart(){
+    this.startGame = true;
+    return this;
+  }
+
+  build(){
+    const game = new Game();
+    game.addPlayers(this.player1, this.player2);
+    if(this.startGame){
+      game.start();
+    }
+    return game;
+  }
+}
+
+module.exports = GameBuilder;
---
+++ tests/components/games/gameBuilder.test.js
@@ -0,0 +1,14 @@
+const GameBuilder = require("../../../components/games/gameBuilder");
+const Player = require("../../../components/players/player");
+const StoneColor = require("../../../components/stoneColors/stoneColor");
+
+
+test('Game builder with players and start game must build correct game', () => {
+  const player1 = new Player('Player 1', StoneColor.black);
+  const player2 = new Player('Player 2', StoneColor.white);
+  const game = new GameBuilder().withPlayers(player1, player2).withStart().build(); 
+  expect(game.player1).toBe(player1);
+  expect(game.player2).toBe(player2);
+  expect(game.currentPlayer).toBe(player1);
+  expect(game.gameWasStarted).toBe(true);
+});
