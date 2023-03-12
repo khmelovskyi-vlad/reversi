06ce55e723baeac88b4249dbbcc2a541d9949863
Khmelovskyi, Vladyslav <vladyslav.khmelovskyi@sigma.software>
2023-03-12 15:31:54 +0200

    Use cell in field

--- components/fields/field.js
+++ components/fields/field.js
@@ -1,9 +1,11 @@
+const Cell = require("../cells/cell");
+
 class Field {
     cells = [];
     create(){
         for (let i = 0; i < 8; i++) {
             for (let j = 0; j < 8; j++) {
-                this.cells.push({x: i, y: j, isEmpty: true, player: null});
+                this.cells.push(new Cell(i, j));
             }
         }
     }
