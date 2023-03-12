a7d905ef66010838bea094727eb0db11af1febcf
Khmelovskyi, Vladyslav <vladyslav.khmelovskyi@sigma.software>
2023-03-12 16:50:35 +0200

    Add initialize players test and realization

--- components/games/game.js
+++ components/games/game.js
@@ -2,8 +2,14 @@ const Field = require("../fields/field");
 
 class Game {
   field = null;
+  player1 = null;
+  player2 = null;
   start(){
     this.field = new Field();
+  }
+  addPlayers(player1, player2){
+    this.player1 = player1;
+    this.player2 = player2;
   }
 }
 
--- tests/components/games/game.test.js
+++ tests/components/games/game.test.js
@@ -1,8 +1,20 @@
 const Game = require("../../../components/games/game");
+const Player = require("../../../components/players/player");
 
 test('Create field on start game', () => {
   const game = new Game();
   expect(game.field).toBe(null);
   game.start();
   expect(game.field).not.toBe(null);
+});
+
+test('Add max two players to game', () => {
+  const game = new Game();
+  expect(game.player1).toBe(null);
+  expect(game.player2).toBe(null);
+  const player1 = new Player();
+  const player2 = new Player(); 
+  game.addPlayers(player1, player2);
+  expect(game.player1).toBe(player1);
+  expect(game.player2).toBe(player2);
 });
