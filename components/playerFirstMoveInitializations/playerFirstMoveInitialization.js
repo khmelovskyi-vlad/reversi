286a3260d15f5e6923d7f226db20bb27911943a5
Khmelovskyi, Vladyslav <vladyslav.khmelovskyi@sigma.software>
2023-03-16 20:24:43 +0200

    Add player first move

---
+++ components/playerFirstMoveInitializations/playerFirstMoveInitialization.js
@@ -0,0 +1,74 @@
+export class PlayerFirstMoveInitialization {
+  static classes = ['col'];
+  static labelClasses = ['form-check-label'];
+  static inputClasses = ['form-check-input'];
+  static inputIdFirstPart = 'playerFirstMove';
+  static labelForAttributeName = 'for';
+  static inputIdAttributeName = 'id';
+  static inputTypeAttributeName = 'type';
+  static inputTypeAttribute = 'ratio';
+  static labelTextFormat = 'Have first move';
+  static inputNameAttributeName = 'name';
+  static inputNameAttribute = 'firstMove';
+  static inputRequiredAttributeName = 'required';
+  static inputRequiredAttribute = '';
+  static inputCheckedAttributeName = 'checked';
+  static inputCheckedAttribute = '';
+  document = null;
+  labelDocument = null;
+  inputDocument = null;
+  
+  constructor(playerNumber){
+    this.playerNumber = playerNumber;
+    if(this.playerNumber === 1){
+      this.checked = true;
+    }
+    this.initDocument();
+    this.initLabel();
+    this.initInput();
+  }
+
+  initDocument(){
+    this.document = document.createElement('div');
+    PlayerFirstMoveInitialization.classes.forEach(oneClass => {
+      this.document.classList.add(oneClass);
+    });
+  }
+
+  initLabel(){
+    this.labelDocument = document.createElement('label');
+    PlayerFirstMoveInitialization.labelClasses.forEach(oneClass => {
+      this.labelDocument.classList.add(oneClass);
+    });
+    this.labelDocument.setAttribute(
+      PlayerFirstMoveInitialization.labelForAttributeName,
+      PlayerFirstMoveInitialization.inputIdFirstPart + this.playerNumber);
+    this.labelDocument.textContent = PlayerFirstMoveInitialization.labelTextFormat;
+    this.document.appendChild(this.labelDocument);
+  }
+
+  initInput(){
+    this.inputDocument = document.createElement('input');
+    PlayerFirstMoveInitialization.inputClasses.forEach(oneClass => {
+      this.inputDocument.classList.add(oneClass);
+    });
+    this.inputDocument.setAttribute(
+      PlayerFirstMoveInitialization.inputIdAttributeName,
+      PlayerFirstMoveInitialization.inputIdFirstPart + this.playerNumber);
+    this.inputDocument.setAttribute(
+      PlayerFirstMoveInitialization.inputTypeAttributeName,
+      PlayerFirstMoveInitialization.inputTypeAttribute);
+    this.inputDocument.setAttribute(
+      PlayerFirstMoveInitialization.inputRequiredAttributeName,
+      PlayerFirstMoveInitialization.inputRequiredAttribute);
+    this.inputDocument.setAttribute(
+      PlayerFirstMoveInitialization.inputNameAttributeName,
+      PlayerFirstMoveInitialization.inputNameAttribute);
+    if (this.checked) {
+      this.inputDocument.setAttribute(
+        PlayerFirstMoveInitialization.inputCheckedAttributeName,
+        PlayerFirstMoveInitialization.inputCheckedAttribute);
+    }
+    this.document.appendChild(this.inputDocument);
+  }
+}
---
+++ tests/components/playerFirstMoveInitializations/playerFirstMoveInitialization.test.js
@@ -0,0 +1,85 @@
+import { PlayerFirstMoveInitialization } from "../../../components/playerFirstMoveInitializations/playerFirstMoveInitialization.js";
+
+test('player first move initialization must create document with correct classes', () => {
+  const playerFirstMoveInitialization = new PlayerFirstMoveInitialization(1); 
+  PlayerFirstMoveInitialization.classes.forEach(oneClass => {
+    expect(playerFirstMoveInitialization.document.classList).toContain(oneClass);
+  });
+});
+
+test('player first move initialization must create label document with correct classes', () => {
+  const playerFirstMoveInitialization = new PlayerFirstMoveInitialization(1);
+  PlayerFirstMoveInitialization.labelClasses.forEach(oneClass => {
+    expect(playerFirstMoveInitialization.labelDocument.classList).toContain(oneClass);
+  });
+});
+
+test('player first move initialization must create label document with correct for', () => {
+  const playerNumber = 1;
+  const playerFirstMoveInitialization = new PlayerFirstMoveInitialization(playerNumber);
+  expect(playerFirstMoveInitialization.labelDocument
+    .getAttribute(PlayerFirstMoveInitialization.labelForAttributeName))
+    .toBe(PlayerFirstMoveInitialization.inputIdFirstPart + playerNumber);
+});
+
+test('player first move initialization must create label document with correct text', () => {
+  const playerNumber = 1;
+  const playerFirstMoveInitialization = new PlayerFirstMoveInitialization(playerNumber);
+  expect(playerFirstMoveInitialization.labelDocument.textContent)
+    .toBe(PlayerFirstMoveInitialization.labelTextFormat);
+});
+
+test('player first move initialization must create input document with correct classes', () => {
+  const playerFirstMoveInitialization = new PlayerFirstMoveInitialization(1);
+  PlayerFirstMoveInitialization.inputClasses.forEach(oneClass => {
+    expect(playerFirstMoveInitialization.inputDocument.classList).toContain(oneClass);
+  });
+});
+
+test('player first move initialization must create input document with correct id', () => {
+  const playerNumber = 1;
+  const playerFirstMoveInitialization = new PlayerFirstMoveInitialization(playerNumber);
+  expect(playerFirstMoveInitialization.inputDocument
+    .getAttribute(PlayerFirstMoveInitialization.inputIdAttributeName))
+    .toBe(PlayerFirstMoveInitialization.inputIdFirstPart + playerNumber);
+});
+
+test('player first move initialization must create input document with correct type', () => {
+  const playerNumber = 1;
+  const playerFirstMoveInitialization = new PlayerFirstMoveInitialization(playerNumber);
+  expect(playerFirstMoveInitialization.inputDocument
+    .getAttribute(PlayerFirstMoveInitialization.inputTypeAttributeName))
+    .toBe(PlayerFirstMoveInitialization.inputTypeAttribute);
+});
+
+test('player first move initialization must create input document with required attribute', () => {
+  const playerNumber = 1;
+  const playerFirstMoveInitialization = new PlayerFirstMoveInitialization(playerNumber);
+  expect(playerFirstMoveInitialization.inputDocument
+    .getAttribute(PlayerFirstMoveInitialization.inputRequiredAttributeName))
+    .toBe(PlayerFirstMoveInitialization.inputRequiredAttribute);
+});
+
+test('player first move initialization must create input document with correct name attribute', () => {
+  const playerNumber = 1;
+  const playerFirstMoveInitialization = new PlayerFirstMoveInitialization(playerNumber);
+  expect(playerFirstMoveInitialization.inputDocument
+    .getAttribute(PlayerFirstMoveInitialization.inputNameAttributeName))
+    .toBe(PlayerFirstMoveInitialization.inputNameAttribute);
+});
+
+test('player first move initialization must create input document with checked attribute if player number is 1', () => {
+  const playerNumber = 1;
+  const playerFirstMoveInitialization = new PlayerFirstMoveInitialization(playerNumber);
+  expect(playerFirstMoveInitialization.inputDocument
+    .getAttribute(PlayerFirstMoveInitialization.inputCheckedAttributeName))
+    .toBe(PlayerFirstMoveInitialization.inputCheckedAttribute);
+});
+
+test('player first move initialization must create input document without checked attribute if player number is not 1', () => {
+  const playerNumber = 20;
+  const playerFirstMoveInitialization = new PlayerFirstMoveInitialization(playerNumber);
+  expect(playerFirstMoveInitialization.inputDocument
+    .getAttribute(PlayerFirstMoveInitialization.inputCheckedAttributeName))
+    .toBe(null);
+});
