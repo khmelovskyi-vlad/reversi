d9f2f0474c4ffbf226e009df077b191cf7f24642
Khmelovskyi, Vladyslav <vladyslav.khmelovskyi@sigma.software>
2023-03-31 21:03:32 +0300

    Set documents in main

--- components/mains/main.js
+++ components/mains/main.js
@@ -2,16 +2,19 @@ import { GameInitialization } from "../gameInitializations/gameInitialization";
 
 export class Main {
   static classes = ['main', 'd-flex', 'justify-content-center'];
+  document = null;
   game = null;
 
   constructor(){
     this.initDocument();
     this.gameInitialization = new GameInitialization(() => this.onInitializeGame());
+    this.document.appendChild(this.gameInitialization.document);
   }
 
   onInitializeGame(){
     try {
       this.game = this.gameInitialization.tryGetValue();
+      this.document.removeChild(this.gameInitialization.document);
     } catch (error) {
       console.error(error);
     }
--- tests/components/mains/main.test.js
+++ tests/components/mains/main.test.js
@@ -11,6 +11,18 @@ test('main should contain game initialization after creating', () => {
   expect(main.gameInitialization).not.toBe(null);
 });
 
+test('main should contain game initialization document in own document after creating', () => {
+  const main = new Main();
+  let contaign = false;
+  for (const child of main.document.children) {
+    if (main.gameInitialization.document === child) {
+      contaign = true;
+      break;
+    }
+  }    
+  expect(contaign).toBe(true);
+});
+
 test('main must create document with correct classes', () => {
   const main = new Main();
   Main.classes.forEach(oneClass => {
@@ -22,4 +34,12 @@ test('gameInitialization submitButton document must create game on click', () =>
   const main = new Main();
   main.gameInitialization.submitButton.document.click();
   expect(main.game).not.toBe(null);
+});
+
+test('gameInitialization submitButton document must remove gameInitialization document', () => {
+  const main = new Main();
+  main.gameInitialization.submitButton.document.click();
+  for (const child of main.document.children) {
+    expect(child).not.toBe(main.gameInitialization.document);
+  }
 });
